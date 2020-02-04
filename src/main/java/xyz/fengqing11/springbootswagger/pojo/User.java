839410562972c50e682fa990bcc9eeccc16deb59
fengqing11 <tanzi8845@gmail.com>
2020-02-04 14:05:16 +0800

    first commit

---
+++ src/main/java/xyz/fengqing11/springbootswagger/pojo/User.java
@@ -0,0 +1,36 @@
+package xyz.fengqing11.springbootswagger.pojo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "用户实体类",description = "用户信息描述类")
+public class User {
+    @ApiModelProperty(value = "用户名")
+    private String username;
+    @ApiModelProperty(value = "地址")
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/xyz/fengqing11/springbootswagger/SpringbootSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.fengqing11.springbootswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
